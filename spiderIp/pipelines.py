d7cb54d08c997de9b97a1c057ca52aeef9cf7aa1
senir <dingxue@zhongan.io>
2017-11-15 10:26:03 +0800

    :fix redis strname:

--- spiderIp/pipelines.py
+++ spiderIp/pipelines.py
@@ -7,7 +7,7 @@
 
 import json
 import codecs
-from spiderIp.redisServer import RedisServer
+from redisServer import RedisServer
 
 class TutorialPipeline(object):
     def process_item(self, item, spider):
@@ -20,7 +20,7 @@ class JsonWriterPipeline(object):
         # self.file = codecs.open('items.json', 'wb', encoding='utf-8')
 
     def process_item(self, dt, spider):
-        self.reids.set('ip', dt)
+        self.redis.set('ip', dt)
         # line = json.dumps(dict(item)) + "\n"
         # self.file.write(line.decode('unicode_escape'))
         return dt
--- spiderIp/redisServer.py
+++ spiderIp/redisServer.py
@@ -5,15 +5,9 @@ import redis
 
 class RedisServer(object):
     def __init__(self):
-        self.cache = redis.Redis(host = 'localhost', port = 6379, db = 0) 
-    def __new__(cls,*args, **kwargs):
-        return object.__new__(cls, *args, **kwargs)  
+        self.cache = redis.Redis(host = 'localhost', port = 6379, db = 0)
 
     def set(self, key, doc):
-        print '-=-=-=-=-=-=-=-='
-        print key
-        print doc
-        print '-=-=-=-=-=-=-=-='
         self.cache.set(key, doc)
 
     def getInstance(self):
